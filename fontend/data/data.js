495b2df638e13122914331fa3d76415022217ee2
Adikumar3135 <adikumar3135@gmail.com>
2025-09-06 00:06:16 +0530

    Add files via upload

---
+++ fontend/data/data.js
@@ -0,0 +1,31 @@
+// data/data.js
+
+const products = [
+  { id: 1, 
+    name: "Classic Tote", 
+    price: 799, 
+    img: "https://5.imimg.com/data5/ZD/PR/DG/SELLER-54158837/canvas-tote-bag-500x500.jpg" 
+  },
+  { id: 2, 
+    name: "Eco Tote", 
+    price: 999, 
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrK5jz1zi4xysreCkdP_CzDa7RmB8A51uWAw&s" 
+  },
+  { id: 3, 
+    name: "Travel Tote", 
+    price: 1199, 
+    img: "https://sumaavi.in/cdn/shop/files/BGP00029.jpg?v=1731566115" 
+  },
+  { id: 4, 
+    name: "Premium Tote", 
+    price: 1499, 
+    img: "https://www.thepurpletree.in/cdn/shop/files/TBAG00315_1.jpg?v=1712844832" 
+  },
+  { id: 5, 
+    name: "Premium Tote", 
+    price: 1599, 
+    img: "https://sumaavi.in/cdn/shop/files/BGP00029.jpg?v=1731566115" 
+  },
+];
+
+// If you host images elsewhere, change the img path accordingly.
